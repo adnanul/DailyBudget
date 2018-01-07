9b6b6780a14bb7b61fcbf85357839c14b8b4c61e
adnan sarkar <adnan@email.com>
2018-01-07 08:39:16 +0600

    commit

---
+++ app/src/main/java/com/dbbl/adnan/dailybudget/activities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dbbl.adnan.dailybudget.activities;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dbbl.adnan.dailybudget.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
